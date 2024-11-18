1f4de7b673a8ebe3821839e4b3829054e6f21423
lspongebobjh <lspongebobjh@gmail.com>
2024-11-19 00:41:49 +0800

    update

--- run1.sh
+++ run1.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-CUDA_VISIBLE_DEVICES=1 python run_wandb.py --wandb sweep --config ./configs/GNN_wandb_1.yaml &
+CUDA_VISIBLE_DEVICES=3 python run_wandb.py --wandb normal --config ./configs/GAT_fuse_embed_best.yaml --project_name exp_1 --seed 0 &
+CUDA_VISIBLE_DEVICES=4 python run_wandb.py --wandb normal --config ./configs/GAT_fuse_embed_best.yaml --project_name exp_1 --seed 1 &
+CUDA_VISIBLE_DEVICES=5 python run_wandb.py --wandb normal --config ./configs/GAT_fuse_embed_best.yaml --project_name exp_1 --seed 2 &
+CUDA_VISIBLE_DEVICES=6 python run_wandb.py --wandb normal --config ./configs/GAT_fuse_embed_best.yaml --project_name exp_1 --seed 3 &
+CUDA_VISIBLE_DEVICES=7 python run_wandb.py --wandb normal --config ./configs/GAT_fuse_embed_best.yaml --project_name exp_1 --seed 4 &
