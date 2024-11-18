5df557a38dc9fc6914c9d8e40e444e84e2316b7f
lspongebobjh <lspongebobjh@gmail.com>
2024-11-18 02:41:31 +0000

    update run_wandb.py

--- run.sh
+++ run.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-wandb sweep --project graph_test configs/GNN.yaml
+# seeds=( {1..5..1} )
+
+# for seed in "${seeds[@]}"; do
+CUDA_VISIBLE_DEVICES=6 python run_wandb.py --wandb normal --config ./configs/GCN_best.yaml --project_name exp_1 --seed 0 &
+CUDA_VISIBLE_DEVICES=6 python run_wandb.py --wandb normal --config ./configs/GCN_best.yaml --project_name exp_1 --seed 1 &
+CUDA_VISIBLE_DEVICES=7 python run_wandb.py --wandb normal --config ./configs/GCN_best.yaml --project_name exp_1 --seed 2 &
+CUDA_VISIBLE_DEVICES=7 python run_wandb.py --wandb normal --config ./configs/GCN_best.yaml --project_name exp_1 --seed 3 &
+CUDA_VISIBLE_DEVICES=7 python run_wandb.py --wandb normal --config ./configs/GCN_best.yaml --project_name exp_1 --seed 4 &
--- run_wandb.py
+++ run_wandb.py
@@ -1,10 +1,21 @@
 import wandb
 import yaml
+from argparse import ArgumentParser
+
+from utils import set_random_seed
 
 project_name = 'graph_test'
 
-def main():
-    wandb.init(project=project_name)
+def modify_project_name(new_project_name):
+    global project_name
+    project_name = new_project_name
+
+def main(seed=0, config=None):
+    set_random_seed(seed)
+    if config is not None:
+        wandb.init(project=project_name, config=config)
+    else:
+        wandb.init(project=project_name)
     config = wandb.config
     if config.model_name in \
         ['MHGCN', 'NeuroPath', 'GCN', 'SAGE', 'SGC', 'GAT', 'Transformer']:
@@ -16,10 +27,25 @@ def main():
     pipe(config)
 
 if __name__ == '__main__':
+    parser = ArgumentParser()
 
+    parser.add_argument('--wandb', type=str, choices=['sweep', 'normal', 'repeat'])
+    parser.add_argument('--config', type=str, default='./configs/GNN.yaml')
+    parser.add_argument('--project_name', type=str, default='graph_test')
+    parser.add_argument('--seed', type=int, default=0)
 
-    with open('./configs/GNN.yaml', 'r') as f:
-        configs = yaml.safe_load(f)
+    args = parser.parse_args()
 
-    sweep_id = wandb.sweep(sweep=configs, project=project_name)
-    wandb.agent(sweep_id, function=main)
+    with open(args.config, 'r') as f:
+        config = yaml.safe_load(f)
+
+    modify_project_name(args.project_name)
+
+    if args.wandb == 'sweep':
+        sweep_id = wandb.sweep(sweep=config, project=project_name)
+        wandb.agent(sweep_id, function=main)
+    elif args.wandb == 'normal':
+        main(args.seed, config=config)
+    elif args.wandb == 'repeat':
+        for seed in range(5):
+            main(seed, config=config)
