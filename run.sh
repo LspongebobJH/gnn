efaaafa47d3ead1c5ee59620f015747d074584b1
lspongebobjh <lspongebobjh@gmail.com>
2024-11-20 22:28:28 +0800

    5 repeat fuse_graph_best exp_1

--- run.sh
+++ run.sh
@@ -9,7 +9,7 @@ for model in "${models[@]}"; do
     CUDA_VISIBLE_DEVICES=5 python run_wandb.py --wandb normal --config ./configs/${model}_fuse_graph_best.yaml --project_name exp_1 --seed 3 &
     CUDA_VISIBLE_DEVICES=6 python run_wandb.py --wandb normal --config ./configs/${model}_fuse_graph_best.yaml --project_name exp_1 --seed 4 &
     wait
-
+done
 # # wait 
 # CUDA_VISIBLE_DEVICES=4 python run_wandb.py --wandb normal --config ./configs/SGC_fuse_embed_best.yaml --project_name exp_1 --seed 0 &
 # CUDA_VISIBLE_DEVICES=5 python run_wandb.py --wandb normal --config ./configs/SGC_fuse_embed_best.yaml --project_name exp_1 --seed 1 &
