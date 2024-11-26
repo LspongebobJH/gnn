de54958493f58c4b02b970459a92f0fc41e089f2
lspongebobjh <lspongebobjh@gmail.com>
2024-11-26 22:09:27 +0800

    10 repat many models exp_2: fix bug

--- run.sh
+++ run.sh
@@ -7,8 +7,7 @@ device=3
 models=( NeuroPath GAT GCN_fuse_embed SAGE_fuse_embed SGC_fuse_embed GAT_fuse_embed )
 for model in "${models[@]}"; do
     for seed in "${seeds[@]}"; do
-        # CUDA_VISIBLE_DEVICES=$device python run_wandb.py --wandb normal --config ./configs/${model}_best.yaml --project_name ${exp} --seed $seed &
-        echo $device
+        CUDA_VISIBLE_DEVICES=$device python run_wandb.py --wandb normal --config ./configs/${model}_best.yaml --project_name ${exp} --seed $seed &
         device=$(( device + 1 ))
         if [ ${device} -eq 8 ]; then
             device=3
