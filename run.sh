15298c703ead3f15578573a5192cc989cdbeded9
lspongebobjh <lspongebobjh@gmail.com>
2024-11-26 23:08:29 +0800

    10 repat many models exp_2: finish remain models

--- run.sh
+++ run.sh
@@ -4,14 +4,13 @@ seeds=( {0..9..1} )
 exp="exp_2"
 device=3
 # models=( MHGCN NeuroPath Mew GCN SAGE SGC GAT GCN_fuse_embed SAGE_fuse_embed SGC_fuse_embed GAT_fuse_embed )
-models=( GAT_fuse_embed )
+models=( MHGCN Mew GCN SAGE SGC )
 for model in "${models[@]}"; do
     for seed in "${seeds[@]}"; do
         CUDA_VISIBLE_DEVICES=$device python run_wandb.py --wandb normal --config ./configs/${model}_best.yaml --project_name ${exp} --seed $seed &
         device=$(( device + 1 ))
         if [ ${device} -eq 8 ]; then
             device=3
-            wait
         fi
     done
     wait
