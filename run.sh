af64660735f9e2b717799ceb8a1fc4b4b87dca61
lspongebobjh <lspongebobjh@gmail.com>
2024-12-11 09:46:11 +0800

    sweep-MewFuseGraph: add self loop

--- run.sh
+++ run.sh
@@ -49,7 +49,7 @@
 # CUDA_VISIBLE_DEVICES=7 python run_wandb.py --wandb normal --config ./configs/GCN_fuse_embed_nosia_best.yaml --project_name exp_1 --seed 3 &
 # CUDA_VISIBLE_DEVICES=7 python run_wandb.py --wandb normal --config ./configs/GCN_fuse_embed_nosia_best.yaml --project_name exp_1 --seed 4 &
 
-CUDA_VISIBLE_DEVICES=6 python run_wandb.py --wandb sweep --config ./configs/GNN_wandb.yaml --project_name MewFuseGraph &
+CUDA_VISIBLE_DEVICES=5 python run_wandb.py --wandb sweep --config ./configs/GNN_wandb.yaml --project_name MewFuseGraph &
 # CUDA_VISIBLE_DEVICES=2 python run_wandb.py --wandb sweep --config ./configs/GNN_wandb_1.yaml --project_name graph_test &
 # CUDA_VISIBLE_DEVICES=6 python run_wandb.py --wandb sweep --config ./configs/GNN_wandb_1.yaml --project_name graph_test &
 # CUDA_VISIBLE_DEVICES=7 python run_wandb.py --wandb sweep --config ./configs/GNN_wandb_2.yaml --project_name graph_test &
